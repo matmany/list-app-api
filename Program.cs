2df78440058c5494631fb6b0b4817783d7ee1743
matmany <matmanymat@gmail.com>
2021-10-18 09:31:08 -0300

    feat(database-config): initial database configure

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using ListApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,16 +9,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "ListApi", Version = "v1" });
+  c.SwaggerDoc("v1", new() { Title = "ListApi", Version = "v1" });
 });
+
+builder.Services.AddDbContext<ListApiContext>(options =>
+    options
+        .UseNpgsql(builder.Configuration.GetConnectionString("ListApiContext"))
+        .UseSnakeCaseNamingConvention()
+        .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+        .EnableSensitiveDataLogging()
+        );
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ListApi v1"));
+  app.UseSwagger();
+  ///app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ListApi v1"));
+  app.UseSwaggerUI(c=> {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "VehicleQuotes v1");
+    c.RoutePrefix = "";
+  });
 }
 
 app.UseHttpsRedirection();
